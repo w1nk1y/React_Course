66903c986a6a4cbd9f7801055b1f0cc102d70e5b
w1nk1y <101086274+w1nk1y@users.noreply.github.com>
2024-12-22 14:37:30 +0600

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+import React, { useState } from 'react';
 import UserCard from './widgets/User/User';
 import UserSkills from './widgets/Skills/Skills';
 import Footer from './widgets/Footer/Footer';
@@ -6,14 +8,37 @@ import skills from './widgets/Skills/skillsData';
 import user from './widgets/User/userData';
 
 const App: React.FC = () => {
-  return (  
-    <div className={styles.container}>
-      <UserCard user={user}/>
+
+  const [state, setState] = useState<string>("white");
+  let themeType;
+
+  const ChangeClick = () => {
+    let newState;
+    if (state === "white") {
+      newState = "black";
+    } 
+    else {
+      newState = "white";
+    }
+    setState(newState);
+  };
+
+  
+  if (state === "white") {
+    themeType = styles.lightTheme;
+  } 
+  else {
+    themeType = styles.darkTheme;
+  }
+
+  return (
+    <div className={`${styles.container} ${themeType}`}>
+      <UserCard user={user} />
       <UserSkills skills={skills} />
+      <button onClick={ChangeClick} className={styles.Button}>Сменить тему</button>
       <Footer />
-  </div>
+    </div>
   );
-
 };
-export default App
 
+export default App;
